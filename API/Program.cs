40a0661135c11980126dbe518f7b1a09d0369fb3
David Stupek <david@Davids-Air.lan>
2023-10-11 00:37:10 -0500

    My End Point Works

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
