2b657b03fa2d135f25cbb6338ce873d2d9d2482e
David Stupek <david@Davids-Air.lan>
2023-12-04 02:23:46 -0600

    EndPointsComplete

--- Domain/CarPart.cs
+++ Domain/CarPart.cs
@@ -9,8 +9,8 @@ namespace Domain
     public class CarPart
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string  Description { get; set; }
-        public string Price { get; set; }
+        public string Name { get; set; } = "";
+        public string  Description { get; set; } = "";
+        public string Price { get; set; } = "";
     }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
