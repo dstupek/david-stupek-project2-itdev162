780a810bb2561747473fb1737756193c00ce218e
David Stupek <david@Davids-Air.lan>
2023-12-04 21:57:54 -0600

    More Angular

---
+++ client/src/app/create-car-part/create-car-part.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-car-part',
+  templateUrl: './create-car-part.component.html',
+  styleUrls: ['./create-car-part.component.css']
+})
+export class CreateCarPartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/view-car-part/view-car-part.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-car-part',
+  templateUrl: './view-car-part.component.html',
+  styleUrls: ['./view-car-part.component.css']
+})
+export class ViewCarPartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
